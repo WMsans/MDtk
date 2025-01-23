4918dacb9f30ccb4166ab00787b6f9a8c6715ffc
WMsans <141427510+WMsans@users.noreply.github.com>
2025-01-23 14:07:03 -0800

    dragable trigger fix

--- Assets/Scripts/LevelDragTrigger.cs
+++ Assets/Scripts/LevelDragTrigger.cs
@@ -1,31 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class LevelDragTrigger : MonoBehaviour
+public class LevelDragTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private CameraDrag cameraDrag;
     [SerializeField] private float lastMaxDeltaTime = 0.1f;
 
     private Vector2 _lastMousePosition;
+    private bool _dragable = false;
     private bool _isDragging = false;
     private float _currentZoom = 1f;
     private Vector2 _lastMaxDelta = Vector2.zero;
     private float _lastMaxDeltaTimer = 0f;
-
     void Update()
     {
         // Check for right mouse button down
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0))
         {
-            _isDragging = true;
-            _lastMousePosition = GetMousePosition();
+            // If the mouse is over this object, we want to start dragging
+            if (_dragable) 
+            {
+                _isDragging = true;
+                _lastMousePosition = GetMousePosition();
+            }
         }
         // Check for right mouse button up
-        else if (Input.GetMouseButtonUp(1))
+        else if (Input.GetMouseButtonUp(0))
         {
-            _isDragging = false;
-            cameraDrag.AddDragVelocity(-_lastMaxDelta);
+            if(_isDragging)
+            {
+                _isDragging = false;
+                cameraDrag.AddDragVelocity(-_lastMaxDelta);
+            }
         }
         // Get the current zoom
         _currentZoom = Camera.main ? Camera.main.orthographicSize : 1f;
@@ -49,5 +57,15 @@ public class LevelDragTrigger : MonoBehaviour
     private Vector2 GetMousePosition()
     {
         return Input.mousePosition * _currentZoom;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _dragable = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _dragable = false;
     }
 }
