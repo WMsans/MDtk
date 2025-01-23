cfa6ac46ce98b40ef6af28ed489c15324e97b663
WMsans <141427510+WMsans@users.noreply.github.com>
2025-01-23 10:16:58 -0800

    Drag slide fix

--- Assets/Scripts/LevelDragTrigger.cs
+++ Assets/Scripts/LevelDragTrigger.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class LevelDragTrigger : MonoBehaviour
 {
     [SerializeField] private CameraDrag cameraDrag;
+    [SerializeField] private float lastMaxDeltaTime = 0.1f;
 
     private Vector2 _lastMousePosition;
     private bool _isDragging = false;
     private float _currentZoom = 1f;
+    private Vector2 _lastMaxDelta = Vector2.zero;
+    private float _lastMaxDeltaTimer = 0f;
 
     void Update()
     {
@@ -22,6 +25,7 @@ public class LevelDragTrigger : MonoBehaviour
         else if (Input.GetMouseButtonUp(1))
         {
             _isDragging = false;
+            cameraDrag.AddDragVelocity(-_lastMaxDelta);
         }
         // Get the current zoom
         _currentZoom = Camera.main ? Camera.main.orthographicSize : 1f;
@@ -30,6 +34,11 @@ public class LevelDragTrigger : MonoBehaviour
         {
             var currentMousePosition = GetMousePosition();
             var mouseDelta = currentMousePosition - _lastMousePosition;
+            if (mouseDelta.sqrMagnitude > _lastMaxDelta.sqrMagnitude || Time.time - _lastMaxDeltaTimer > lastMaxDeltaTime)
+            {
+                _lastMaxDelta = mouseDelta;
+                _lastMaxDeltaTimer = Time.time;
+            }
             _lastMousePosition = currentMousePosition;
 
             // We want the camera to move in the OPPOSITE direction of the mouse drag
