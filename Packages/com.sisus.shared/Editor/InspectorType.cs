7ee052e8fd4fa9e2db8c5254b8247e3e21b9d97c
WMsans <ggajy123@gmail.com>
2025-01-22 18:40:52 -0800

    Initial commit

---
+++ Packages/com.sisus.shared/Editor/InspectorType.cs
@@ -0,0 +1,15 @@
+﻿#define DEBUG_REPAINT
+
+
+#if DEV_MODE && DEBUG && !INIT_ARGS_DISABLE_PROFILING
+#endif
+
+namespace Sisus.Shared.EditorOnly
+{
+	public enum InspectorType
+	{
+		InspectorWindow,
+		InspectorWindowDebugMode,
+		PropertiesWindow
+	}
+}
